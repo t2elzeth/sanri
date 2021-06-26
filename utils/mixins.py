bfd6ca3e2c9961c10d8e6ecd3646be7c738e0872
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-06-26 16:46:16 +0600

    Update

--- utils/mixins.py
+++ utils/mixins.py
@@ -14,4 +14,4 @@ class DetailAPIViewMixin(
         return self.update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
-        return self.delete(request, *args, **kwargs)
+        return self.destroy(request, *args, **kwargs)
