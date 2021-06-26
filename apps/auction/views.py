f709f54d963648df73300daed4ab6ced3afdf8b2
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-06-26 22:20:18 +0600

    Update

--- apps/auction/views.py
+++ apps/auction/views.py
@@ -7,6 +7,7 @@ from .serializers import AuctionSerializer
 
 
 class AuctionAPIView(generics.ListCreateAPIView):
+    """Создать новый или получить список аукционов"""
     queryset = Auction.objects.all()
     serializer_class = AuctionSerializer
 
