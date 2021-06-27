e5bff93b88ac7f999f6b6ac65b2ae8624cfbfb9b
Ulukman Amangeldiev <t2elzeth@134-0-116-226.xen.vps.regruhosting.ru>
2021-06-27 07:54:50 +0000

    Update

--- apps/container/admin.py
+++ apps/container/admin.py
@@ -8,12 +8,14 @@ class CountAndSumInline(admin.StackedInline):
     extra = 0
 
     def has_add_permission(self, request, obj):
-        return False
+        return request.user.is_superuser 
 
     def has_delete_permission(self, request, obj=None):
-        return False
+        return request.user.is_superuser 
 
 
 @admin.register(models.Container)
 class ContainerAdmin(admin.ModelAdmin):
+    list_display = ("id", "name") 
+
     inlines = [CountAndSumInline]
