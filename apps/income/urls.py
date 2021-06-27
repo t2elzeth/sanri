60b8e68d590b138b28eeca255bdcb53384e2800a
Ulukman Amangeldiev <t2elzeth@134-0-116-226.xen.vps.regruhosting.ru>
2021-06-27 07:37:19 +0000

    Update

--- apps/income/urls.py
+++ apps/income/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.IncomeAPIView.as_view()),
     path("<int:pk>/", views.IncomeDetailAPIView.as_view()),
-    path("Type/", views.IncomeAPIView.as_view()),
-    path("Type/<int:pk>/", views.IncomeDetailAPIView.as_view()),
+    path("Type/", views.IncomeTypeAPIView.as_view()),
+    path("Type/<int:pk>/", views.IncomeTypeDetailAPIView.as_view()),
 ]
