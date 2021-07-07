7fdbb1ae5dc38edbfa93b49484dadb50707f668e
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-07 22:35:31 +0600

    Bug fix

--- apps/monthly_payment/models.py
+++ apps/monthly_payment/models.py
@@ -6,7 +6,7 @@ class MonthlyPaymentType(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
 class MonthlyPayment(models.Model):
