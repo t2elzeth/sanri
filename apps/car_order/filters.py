d5e4f64ba720ac9641849a70d3649b215fcc2c7d
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-11 01:16:55 +0600

    Update

--- apps/car_order/filters.py
+++ apps/car_order/filters.py
@@ -5,6 +5,7 @@ from .models import CarOrder
 
 class CarModelFilter(django_filters.rest_framework.FilterSet):
     of_client = django_filters.CharFilter(field_name='client__id', lookup_expr="iexact")
+    by_mark = django_filters.CharFilter(field_name='carModel__mark__name')
 
     class Meta:
         model = CarOrder
