fe4e3e191e15232be04eb3e7a97f9e57c3c01314
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-10 19:10:04 +0600

    Update

--- apps/car_model/models.py
+++ apps/car_model/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 
-class CarModel(models.Model):
-    mark = models.CharField(max_length=255)
-    model = models.CharField(max_length=255)
+class CarMark(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.mark}:{self.model}"
+        return self.name
+
+
+class CarModel(models.Model):
+    mark = models.ForeignKey(CarMark, on_delete=models.CASCADE, related_name='models')
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.mark}:{self.name}"
