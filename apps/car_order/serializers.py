a3526b14a63482caedce128dc5788c4daba8ebf9
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-11 00:04:06 +0600

    Update

--- apps/car_order/serializers.py
+++ apps/car_order/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
 
 from .models import CarOrder
+from car_model.models import CarModel
 from authorization.models import User
+
+
+class CarModelSerializer(CarModel):
+    class Meta:
+        model = CarModel
+        fields = ['id', 'mark', 'name']
+        depth = 2
 
 
 class CarOrderSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.fullName', read_only=True)
     marka_name = serializers.SerializerMethodField(read_only=True)
     auction_name = serializers.CharField(source="auction.name", read_only=True)
+    carModel = CarModelSerializer()
 
     def get_marka_name(self, obj):
         return f'{obj.carModel.mark.name} / {obj.carModel.name}'
