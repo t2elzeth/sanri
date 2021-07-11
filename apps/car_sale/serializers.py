780fdb21e5f853f7e66159cffe419739dc22b9d1
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-11 18:09:37 +0600

    Update

--- apps/car_sale/serializers.py
+++ apps/car_sale/serializers.py
@@ -1,14 +1,29 @@
 from rest_framework import serializers
 
+from authorization.models import User
 from .models import CarSale
 
 
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'fullName']
+        ref_name = 'car_sale'
+
+
 class CarSaleSerializer(serializers.ModelSerializer):
+    ownerClient_id = serializers.PrimaryKeyRelatedField(source="ownerClient",
+                                                        write_only=True,
+                                                        queryset=User.objects.all())
+
+    ownerClient = ClientSerializer(read_only=True)
+
     class Meta:
         model = CarSale
         fields = [
             "id",
             "ownerClient",
+            "ownerClient_id",
             "auction",
             "carModel",
             "vinNumber",
