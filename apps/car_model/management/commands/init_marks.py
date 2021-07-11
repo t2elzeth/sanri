5d303240ae08ff784daf64dac01aeed28e856281
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-11 13:18:49 +0600

    Update

---
+++ apps/car_model/management/commands/init_marks.py
@@ -0,0 +1,62 @@
+from django.core.management.base import BaseCommand
+from car_model.models import CarMark
+marks = """
+TOYOTA
+NISSAN
+MAZDA
+MITSUBISHI
+HONDA
+SUZUKI
+SUBARU
+ISUZU
+DAIHATSU
+MITSUOKA
+LEXUS
+AUDI
+BENTLEY
+BMW
+CADILLAC
+CHEVROLET
+CHRYSLER
+CITROEN
+DODGE
+FERRARI
+FIAT
+FORD
+GM
+GMC
+HINO
+HUMMER
+INFINITI
+JAGUAR
+JEEP
+KAWASAKI
+KOMATSU
+LAND ROVER
+LOTUS
+MASERATI
+MERCEDES BENZ
+OPEL
+PEUGEOT
+PORSCHE
+RENAULT
+TADANO
+TCM
+TESLA
+VOLKSWAGEN
+VOLVO
+YANMAR
+"""
+
+marks = [mark for mark in marks.split() if len(mark) > 1 ]
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        counter = 0
+        for mark in marks:
+            car_mark = CarMark.objects.create(name=mark)
+            print(f'Created CarMark: {car_mark.name}')
+            counter += 1
+
+        print(f'Created {counter} marks in total')
