8be81477b0f02f404a9b9f727d34f6889d4205af
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-11 17:47:54 +0600

    Update

--- apps/car_order/serializers.py
+++ apps/car_order/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 
+from authorization.models import User
 from car_model.models import CarModel, CarMark
 from .models import CarOrder
+from auction.models import Auction
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'fullName']
 
 
 class CarOrderMarkSerializer(serializers.ModelSerializer):
@@ -18,12 +26,32 @@ class CarOrderModelSerializer(serializers.ModelSerializer):
         fields = ['id', 'mark', 'name']
 
 
+class AuctionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Auction
+        fields = ['id', 'name']
+
+
 class CarOrderSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.fullName', read_only=True)
     marka_name = serializers.SerializerMethodField(read_only=True)
     auction_name = serializers.CharField(source="auction.name", read_only=True)
     carModelDetail = CarOrderModelSerializer(source='carModel', read_only=True)
-    carModel_id = serializers.IntegerField(source='carModel', write_only=True)
+    carModel_id = serializers.PrimaryKeyRelatedField(source='carModel',
+                                                     write_only=True,
+                                                     queryset=CarModel.objects.all())
+
+    client_id = serializers.PrimaryKeyRelatedField(source="client",
+                                                   write_only=True,
+                                                   queryset=User.objects.all())
+
+    auction_id = serializers.PrimaryKeyRelatedField(source='auction',
+                                                    write_only=True,
+                                                    queryset=Auction.objects.all())
+
+    client = ClientSerializer(read_only=True)
+    auction = AuctionSerializer(read_only=True)
+    carModel = CarOrderModelSerializer(read_only=True)
 
     def get_marka_name(self, obj):
         return f'{obj.carModel.mark.name} / {obj.carModel.name}'
@@ -33,7 +61,10 @@ class CarOrderSerializer(serializers.ModelSerializer):
         fields = [
             "id",
             "client",
+            'client_id',
             'client_name',
+            'auction_id',
+            'carModel',
             'marka_name',
             'auction_name',
             "auction",
