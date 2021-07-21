c642dc7adaf8882d4368842aa7ae1cee33dc9a32
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-21 17:42:41 +0600

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 python manage.py initadmin
+python manage.py initmarks
 
 gunicorn "$DJANGO_SETTINGS_FOLDER".wsgi
