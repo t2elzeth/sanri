4c940f7376f2dff55cca31f363cbbb17e3bc9fee
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-07-23 09:56:44 +0600

    Update

--- apps/transport_companies/serializers.py
+++ apps/transport_companies/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from car_order.serializers import CarOrderSerializer
 from .models import TransportCompany
 
 
 class TransportCompanySerializer(serializers.ModelSerializer):
+    cars = CarOrderSerializer(source="car_orders", many=True, read_only=True)
+
     class Meta:
         model = TransportCompany
-        fields = ["id", "name"]
+        fields = ["id", "name", "cars"]
