5ce13ceaa83afe232116299ed0a5cbbff4d74086
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-08-18 22:16:53 +0600

    Update

--- apps/car_order/models.py
+++ apps/car_order/models.py
@@ -87,6 +87,9 @@ class BalanceWithdrawal(models.Model):
     )
 
     def calculate_amount(self):
+        if self.car_order.client is None:
+            return
+
         if self.car_order.client.atWhatPrice == User.AT_WHAT_PRICE_BY_FACT:
             self.balance.sum_in_jpy = self.car_order.total
         elif  self.car_order.client.atWhatPrice == User.AT_WHAT_PRICE_BY_FOB:
