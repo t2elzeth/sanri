f69b3f2d6a3d25f3d91dd9aa412c1c267e5c5230
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-08-24 19:02:58 +0600

    Update

--- apps/car_resale/filters.py
+++ apps/car_resale/filters.py
@@ -3,10 +3,10 @@ import django_filters
 
 class CarResaleFilter(django_filters.rest_framework.FilterSet):
     mark = django_filters.CharFilter(
-        field_name="carModel__mark__id", lookup_expr="iexact"
+        field_name="carOrder__carModel__mark__id", lookup_expr="iexact"
     )
     model = django_filters.CharFilter(
-        field_name="carModel__id", lookup_expr="iexact"
+        field_name="carOrder__carModel__id", lookup_expr="iexact"
     )
     date_from = django_filters.DateFilter(
         field_name="created_at", lookup_expr="gte"
