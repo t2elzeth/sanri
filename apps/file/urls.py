123f345f6aea558e260e979180c3d79e9a4b356f
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-08-31 15:10:30 +0600

    Update

--- apps/file/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.FileModelAPIView.as_view()),
-    path("<int:pk>/", views.FileModelDetailAPIView.as_view()),
-]
