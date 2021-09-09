686423499de412f0562c53c76b47886de73d771e
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-09-09 22:47:14 +0600

    Update

--- src/routes/routeNames.js
+++ src/routes/routeNames.js
@@ -1,6 +1,12 @@
 export const LOGIN = "/login";
 export const LOGOUT = "/logout";
+export const RESELL_HISTORY = "/resell-history";
+export const NUMBER_AND_DOCUMENTS = "/number-documents";
+export const SOLD_CARS_LIST = "/sold-cars-list";
+export const BUY = "/buy";
+export const SELL = "/sell";
 
 export default {
-  LOGIN, LOGOUT
+  LOGIN, LOGOUT, RESELL_HISTORY, NUMBER_AND_DOCUMENTS, SOLD_CARS_LIST,
+  BUY, SELL
 };
