7a409095f878598eda7e5f26889299e2e8e72b47
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-06 17:14:50 +0600

    Allow analysis field to be blank

--- apps/car_order/models.py
+++ apps/car_order/models.py
@@ -56,7 +56,7 @@ class CarOrder(models.Model):
     total_FOB = models.IntegerField()
     total_FOB2 = models.IntegerField(default=0)
     created_at = models.DateField(default=get_date)
-    analysis = models.JSONField(default=dict)
+    analysis = models.JSONField(default=dict, blank=True)
     comment = models.TextField(default="", blank=True, null=True)
     additional_expenses = models.IntegerField(default=0)
 
