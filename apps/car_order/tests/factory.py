6190cc1dffa44ece5fb3e0f0161e7f717ad25855
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-06 15:50:06 +0600

    Use CarOrderFactory

---
+++ apps/car_order/tests/factory.py
@@ -0,0 +1,28 @@
+import factory
+
+from auction.tests.factory import AuctionFactory
+from car_model.tests.factory import CarModelFactory
+from car_order.models import CarOrder
+from authorization.tests.factory import ClientFactory
+from transport_companies.tests.factory import TransportCompanyFactory
+
+
+class CarOrderFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = CarOrder
+
+    client = factory.SubFactory(ClientFactory)
+    auction = factory.SubFactory(AuctionFactory)
+    lotNumber = factory.Faker("pyint", min_value=1, max_value=10)
+    carModel = factory.SubFactory(CarModelFactory)
+    vinNumber = factory.Faker("bothify", text="CarVin: ????-###?#?#")
+    year = factory.Faker("year")
+    price = factory.Faker("pyint", min_value=1, max_value=100_000_000)
+    recycle = factory.Faker("pyint", min_value=1, max_value=100_000)
+    auctionFees = factory.Faker("pyint", min_value=1, max_value=1_000_000)
+    transport = factory.Faker("pyint", min_value=1, max_value=10_000)
+    amount = factory.Faker("pyint", min_value=1, max_value=100)
+    transportCompany = factory.SubFactory(TransportCompanyFactory)
+
+    carNumber = factory.Iterator(CarOrder.CAR_NUMBER_STATUS_CHOICES)
+    documentsGiven = False
--- apps/car_order/tests/test_models.py
+++ apps/car_order/tests/test_models.py
@@ -1,59 +1,24 @@
 from django.test import TestCase
 
-from car_order.models import CarOrder
-from authorization.models import User
 from auction.models import Auction
-from car_model.models import CarMark, CarModel
+from authorization.models import User
+from car_model.models import CarMark
+from car_model.tests.factory import CarMarkFactory
+from car_order.models import CarOrder
 from transport_companies.models import TransportCompany
+from .factory import CarOrderFactory, ClientFactory, AuctionFactory, TransportCompanyFactory
 
 
 class TestCarOrderAndUserRelation(TestCase):
     def setUp(self) -> None:
-        self.user = User.objects.create_user(
-            password="123",
-            fullName="My owner client",
-            country="KG",
-            email="ownerclient@gmail.com",
-            phoneNumber="+996771221103",
-            service=User.SERVICE_ENTIRE,
-            atWhatPrice=User.AT_WHAT_PRICE_BY_FOB,
-            username="client_by_fob",
-            sizeFOB=25000,
-        )
-
-        self.auction = Auction.objects.create(
-            name="MyTestAuction",
-            parkingPrice1=25000,
-            parkingPrice2=25000,
-            parkingPrice3=25000,
-            parkingPrice4=25000,
-        )
-
-        self.car_marks = {"honda": CarMark.objects.create(name="HONDA")}
+        self.user = ClientFactory.create(sizeFOB=25000)
+        self.auction = AuctionFactory.create()
+        self.car_marks = {"honda": CarMarkFactory.create(name="HONDA")}
         self.car_models = {
             "fit": self.car_marks["honda"].models.create(name="FIT")
         }
-
-        self.transport_company = TransportCompany.objects.create(
-            name="MyTestTransportCompany"
-        )
-
-        self.car_order = CarOrder.objects.create(
-            client=self.user,
-            auction=self.auction,
-            lotNumber=10000,
-            carModel=self.car_models["fit"],
-            vinNumber="vx-1000",
-            year=2015,
-            price=20000,
-            recycle=10000,
-            auctionFees=20000,
-            transport=25000,
-            amount=2,
-            transportCompany=self.transport_company,
-            carNumber=CarOrder.CAR_NUMBER_REMOVED,
-            documentsGiven=False,
-        )
+        self.transport_company = TransportCompanyFactory.create()
+        self.car_order = CarOrderFactory.create(client=self.user)
 
     def test_fob_when_created(self):
         """
@@ -94,52 +59,18 @@ class TestCarOrderAndUserRelation(TestCase):
 
 class TestCarOrderBalanceWithdrawal(TestCase):
     def setUp(self) -> None:
-        self.user = User.objects.create_user(
-            password="123",
-            fullName="My owner client",
-            country="KG",
-            email="ownerclient@gmail.com",
-            phoneNumber="+996771221103",
-            service=User.SERVICE_ENTIRE,
-            atWhatPrice=User.AT_WHAT_PRICE_BY_FOB,
-            username="client_by_fob",
-            sizeFOB=25000,
-        )
+        self.user = ClientFactory.create(sizeFOB=25000)
 
-        self.auction = Auction.objects.create(
-            name="MyTestAuction",
-            parkingPrice1=25000,
-            parkingPrice2=25000,
-            parkingPrice3=25000,
-            parkingPrice4=25000,
-        )
+        self.auction = AuctionFactory.create()
 
-        self.car_marks = {"honda": CarMark.objects.create(name="HONDA")}
+        self.car_marks = {"honda": CarMarkFactory.create(name="HONDA")}
         self.car_models = {
             "fit": self.car_marks["honda"].models.create(name="FIT")
         }
 
-        self.transport_company = TransportCompany.objects.create(
-            name="MyTestTransportCompany"
-        )
+        self.transport_company = TransportCompanyFactory.create()
 
-        self.car_order = CarOrder.objects.create(
-            client=self.user,
-            auction=self.auction,
-            lotNumber=10000,
-            carModel=self.car_models["fit"],
-            vinNumber="vx-1000",
-            year=2015,
-            price=20000,
-            recycle=10000,
-            auctionFees=20000,
-            transport=25000,
-            amount=2,
-            transportCompany=self.transport_company,
-            carNumber=CarOrder.CAR_NUMBER_REMOVED,
-            documentsGiven=False,
-        )
-
+        self.car_order = CarOrderFactory.create(client=self.user)
         self.withdrawal = self.car_order.withdrawal
 
     def test_initial_car_order_withdrawal_amount(self):
