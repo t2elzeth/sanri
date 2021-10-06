8dfc1510bc144d11a918caa514dbf53125b4196c
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-06 15:49:42 +0600

    Create ClientFactory

---
+++ apps/authorization/tests/factory.py
@@ -0,0 +1,19 @@
+import factory
+
+from authorization.models import User
+
+
+class ClientFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    fullName = factory.Faker("name")
+    country = "KG"
+    email = "client@gmail.com"
+    phoneNumber = factory.Faker("phone_number")
+
+    service = User.SERVICE_ENTIRE
+    atWhatPrice = User.AT_WHAT_PRICE_BY_FACT
+    sizeFOB = factory.Faker("numerify", text="###%")
+    username = factory.Faker("user_name")
+    user_type = User.USER_TYPE_CLIENT
--- apps/authorization/tests/test_models.py
+++ apps/authorization/tests/test_models.py
@@ -1,30 +1,12 @@
 from authorization.models import User
 from django.test import TestCase
+from authorization.tests.factory import ClientFactory
 
 
 class TestClientCreate(TestCase):
     def setUp(self) -> None:
-        self.client_by_fact = User.objects.create_user(
-            password="123",
-            fullName="My owner client",
-            country="KG",
-            email="ownerclient@gmail.com",
-            phoneNumber="+996771221103",
-            service=User.SERVICE_ENTIRE,
-            atWhatPrice=User.AT_WHAT_PRICE_BY_FACT,
-            username="client_by_fact",
-        )
-
-        self.client_by_fob = User.objects.create_user(
-            password="123",
-            fullName="My owner client",
-            country="KG",
-            email="ownerclient@gmail.com",
-            phoneNumber="+996771221103",
-            service=User.SERVICE_ENTIRE,
-            atWhatPrice=User.AT_WHAT_PRICE_BY_FOB,
-            username="client_by_fob",
-        )
+        self.client_by_fact = ClientFactory.create(atWhatPrice=User.AT_WHAT_PRICE_BY_FACT)
+        self.client_by_fob = ClientFactory.create(atWhatPrice=User.AT_WHAT_PRICE_BY_FOB)
 
     def test_try_to_change_fob_for_by_fact_client(self):
         self.assertEqual(self.client_by_fact.sizeFOB, 0)
