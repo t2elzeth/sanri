cfe0d3c50e4a4543544ffd7857787b2111665a92
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-07 23:40:00 +0600

    Create setters for works_by

--- apps/authorization/model_helpers.py
+++ apps/authorization/model_helpers.py
@@ -33,13 +33,40 @@ class UserWorksBy:
         self._user = user
 
     @property
+    def __default(self):
+        return self._user.AT_WHAT_PRICE_BY_FACT
+
+    @property
     def by_fact(self) -> bool:
         return self._user.atWhatPrice == self._user.AT_WHAT_PRICE_BY_FACT
+
+    @by_fact.setter
+    def by_fact(self, state: bool):
+        self._user.atWhatPrice = self._user.AT_WHAT_PRICE_BY_FACT
+        self._user.save()
 
     @property
     def by_fob(self) -> bool:
         return self._user.atWhatPrice == self._user.AT_WHAT_PRICE_BY_FOB
 
+    @by_fob.setter
+    def by_fob(self, state: bool):
+        new_value = self._user.AT_WHAT_PRICE_BY_FOB
+        if not state:
+            new_value = self.__default
+
+        self._user.atWhatPrice = new_value
+        self._user.save()
+
     @property
     def by_fob2(self) -> bool:
         return self._user.atWhatPrice == self._user.AT_WHAT_PRICE_BY_FOB2
+
+    @by_fob2.setter
+    def by_fob2(self, state: bool):
+        new_value = self._user.AT_WHAT_PRICE_BY_FOB2
+        if not state:
+            new_value = self.__default
+
+        self._user.atWhatPrice = new_value
+        self._user.save()
