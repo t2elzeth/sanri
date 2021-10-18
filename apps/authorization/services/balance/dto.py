32fa4b3cc30514e54804e50c6ee3d6be5aa4cea2
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-10-18 14:01:41 +0600

    Fix not required

--- apps/authorization/services/balance/dto.py
+++ apps/authorization/services/balance/dto.py
@@ -5,10 +5,10 @@ from typing import Optional
 @dataclass
 class CreateBalanceRecordDTO:
     client_id: int
-    date: Optional[str]
     sum_in_jpy: int
-    sum_in_usa: Optional[int]
-    rate: Optional[int]
     payment_type: str
     sender_name: str
     comment: str
+    date: Optional[str] = None
+    sum_in_usa: Optional[int] = None
+    rate: Optional[int] = None
