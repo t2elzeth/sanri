9b8e535050c0308c0447446c7ccb457007de9500
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-11-04 17:39:30 +0600

    BugFix: Car is sold as created sold car instance

--- apps/car_sale/signals.py
+++ apps/car_sale/signals.py
@@ -21,14 +21,11 @@ def update_stock(instance: CarSale, **kwargs):
 @receiver(post_save, sender=CarSale)
 def post_save_car_resale(instance: CarSale, created, **kwargs):
     if created:
+        instance.carOrder.is_sold = True
+        instance.carOrder.save()
         instance.save()
 
     if instance.status and instance.carOrder is not None:
-        # CarOrder is no longer in DB, cause it's been sold
-        instance.carOrder.is_sold = True
-        instance.carOrder.save()
-
-        # ownerClient receives money for selling his car
         Balance.objects.create(
             client=instance.ownerClient,
             sum_in_jpy=instance.total,
